f05bdb98684e94a0da0ccd0957c1f6eeb30335bc
Valentin <4267817@mail.ru>
2024-04-09 22:07:21 -0700

    add_new

--- test.py
+++ test.py
@@ -1,8 +1,33 @@
-nums = [3,4,5,2]
-num = sorted(nums, reverse=True)
-i=0
-j=1
+left = 1
+right = 22
+# p=[]
+# for i in range(left,right+1):
+#     is_t = True
+#     for t in str(i):
+#         if t == '0' or i % int(t) != 0:
+#             is_t = False
+#             break
+#     if is_t:
 
-# return (num[i]-1)*(num[j]-1)
+#         p.append(i)
+# print(p)
+
+self_dividing_numbers = []
+for num in range(left, right + 1):
+    is_self_dividing = True
+          
+    for digit_str in str(num):
+        digit = int(digit_str)
+        if digit == 0 or num % digit != 0:
+            is_self_dividing = False
+            break
+    if is_self_dividing:
+        self_dividing_numbers.append(num)
+      
+
+print(self_dividing_numbers)
 
 
+# print(list(set(n for n in p if p.count(n) > 1)))
+
+# print([num for num in range(left, right + 1) if all(i != '0' and num % int(i) == 0 for i in str(num))])
